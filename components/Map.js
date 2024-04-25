9035221c843d787b0a35909dc5b7b27dce795b2d
ifedayoagboola19 <ifedayoagboola19@gmail.com>
2024-04-25 23:53:26 +0100

    daily push

--- components/Map.js
+++ components/Map.js
@@ -3,7 +3,17 @@ import MapView from "react-native-maps";
 import { StyleSheet, View } from "react-native";
 
 const Map = () => {
-  return <MapView style={styles.map} />;
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
