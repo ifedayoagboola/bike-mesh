3103009dbf823c83a39fa52adcd16abb7d793b44
ifedayoagboola19 <ifedayoagboola19@gmail.com>
2024-05-02 14:26:52 +0100

    splash loader & upload to expo app store

--- components/Map.js
+++ components/Map.js
@@ -1,10 +1,11 @@
 import React from "react";
-import MapView from "react-native-maps";
-import { StyleSheet, View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { StyleSheet } from "react-native";
 
 const Map = () => {
   return (
     <MapView
+      provider={PROVIDER_GOOGLE}
       style={styles.container}
       initialRegion={{
         latitude: 37.78825,
