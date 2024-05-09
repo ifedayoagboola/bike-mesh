05cb39d936880a89523d152adf5b57841ee06b00
ifedayoagboola19 <ifedayoagboola19@gmail.com>
2024-05-09 17:47:18 +0100

    trying to resolve with unmatched route

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -2,12 +2,12 @@ import { Redirect, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 import { Loader } from "../../components";
-// import { useGlobalContext } from "../../context/GlobalProvider";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const AuthLayout = () => {
-  // const { loading, isLogged } = useGlobalContext();
+  const { loading, isLogged } = useGlobalContext();
 
-  // if (!loading && isLogged) return <Redirect href="/home" />;
+  if (!loading && isLogged) return <Redirect href="/home" />;
 
   return (
     <>
@@ -26,7 +26,7 @@ const AuthLayout = () => {
         />
       </Stack>
 
-      {/* <Loader isLoading={loading} /> */}
+      <Loader isLoading={loading} />
       <StatusBar backgroundColor="#161622" style="light" />
     </>
   );
--- app/[...unmatched].jsx
+++ app/[...unmatched].jsx
@@ -1,3 +1,32 @@
-import { Unmatched } from "expo-router";
+import { Stack } from "expo-router";
+import { Text, View, Image } from "react-native";
 
-export default Unmatched;
+export default function Unmatched() {
+  //? Render(s)
+  return (
+    // <>
+    //   <Stack.Screen
+    //     options={{
+    //       title: "404 Not Found!",
+    //       headerBackTitleVisible: false,
+    //     }}
+    //   />
+    //   <View className="flex h-full flex-col items-center justify-center py-8 gap-y-6 bg-white">
+    //     <Text className="text-base font-semibold text-black">
+    //       404 Not Found!
+    //     </Text>
+    //     <Image
+    //       style={{
+    //         width: "100%",
+    //         resizeMode: "contain",
+    //       }}
+    //       source={require("@/assets/page-not-found.png")}
+    //       alt="404"
+    //     />
+    //       </View>
+
+    // </>
+
+    <Redirect href="/home" />
+  );
+}
