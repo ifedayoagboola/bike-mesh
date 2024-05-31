3e8ecec80302b2e187bc55411c2dd566efaf1083
ifedayoagboola19 <ifedayoagboola19@gmail.com>
2024-05-31 02:29:35 +0100

    removed update url

--- app/(tabs)/history/[id].jsx
+++ app/(tabs)/history/[id].jsx
@@ -1,13 +1,60 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { useRoute } from "@react-navigation/native";
+import { Stack } from "expo-router";
 
 const HistoryDetails = () => {
+  const assets = [
+    {
+      id: 1,
+      assetName: "Giant TCR Advanced",
+      assetType: "bicycle",
+      lastSeen: "Near Duncregan Village centre Since 01:52pm",
+      icon: "bicycle",
+    },
+    {
+      id: 2,
+      assetName: "Giant TCR Advanced",
+      assetType: "bicycle",
+      lastSeen: "Near Duncregan Village centre Since 01:52pm",
+      icon: "bicycle",
+    },
+    {
+      id: 3,
+      assetName: "Giant TCR Advanced",
+      assetType: "bicycle",
+      lastSeen: "Near Duncregan Village centre Since 01:52pm",
+      icon: "bicycle",
+    },
+    {
+      id: 4,
+      assetName: "Giant TCR Advanced",
+      assetType: "bicycle",
+      lastSeen: "Near Duncregan Village centre Since 01:52pm",
+      icon: "bicycle",
+    },
+    {
+      id: 5,
+      assetName: "Giant TCR Advanced",
+      assetType: "bicycle",
+      lastSeen: "Near Duncregan Village centre Since 01:52pm",
+      icon: "bicycle",
+    },
+    {
+      id: 6,
+    },
+  ];
+
   const route = useRoute();
   const { id } = route.params;
 
+  const header = assets.filter((item) => {
+    return id == item.id;
+  });
+
   return (
     <View>
+      <Stack.Screen options={{ headerTitle: header[0].assetName }} />
       <Text>History Details Page</Text>
       <Text>History ID: {id}</Text>
     </View>
--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -10,6 +10,20 @@ const GlobalProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
   useEffect(() => {
     getCurrentUser()
       .then((res) => {
@@ -27,6 +41,7 @@ const GlobalProvider = ({ children }) => {
       .finally(() => {
         setIsLoading(false);
       });
+    onFetchUpdateAsync();
   }, []);
 
   return (
